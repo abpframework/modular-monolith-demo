ba125d5aa5a09edee4290a64957c030ac15a3e1b
Yunus Emre Kalkan <emre.kalkan@volosoft.com>
2025-08-21 11:27:16 +0300

    better handle post-order payment operations

---
+++ Shopularity/modules/shopularity.payment/Shopularity.Payment/Payments/PaymentFakeEventService.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Threading.Tasks;
+using Shopularity.Payment.Payments.Events;
+using Volo.Abp.DependencyInjection;
+using Volo.Abp.EventBus.Distributed;
+
+namespace Shopularity.Payment.Payments;
+
+public class PaymentFakeEventService: ISingletonDependency
+{
+    private readonly IPaymentRepository _paymentRepository;
+    private readonly IDistributedEventBus _eventBus;
+
+    public PaymentFakeEventService(IPaymentRepository paymentRepository, IDistributedEventBus eventBus)
+    {
+        _paymentRepository = paymentRepository;
+        _eventBus = eventBus;
+    }
+    
+    public async Task CompletePaymentAsync(Guid paymentId, int delaySeconds = 60)
+    {
+        _ = Task.Run(async () =>
+        {
+            await Task.Delay(1000 * delaySeconds);
+            
+            var payment = await _paymentRepository.GetAsync(paymentId);
+
+            if (payment.State != PaymentState.Waiting)
+            {
+                return;
+            }
+            
+            payment.State = PaymentState.Completed;
+            
+            await _paymentRepository.UpdateAsync(payment);
+
+            await _eventBus.PublishAsync(new PaymentCompletedEto
+            {
+                PaymentId = paymentId,
+                OrderId = payment.OrderId
+            });
+        });
+    }
+}
--- Shopularity/modules/shopularity.payment/Shopularity.Payment/Payments/PaymentManager.cs
+++ Shopularity/modules/shopularity.payment/Shopularity.Payment/Payments/PaymentManager.cs
@@ -12,15 +12,18 @@ namespace Shopularity.Payment.Payments
         protected IPaymentRepository _paymentRepository;
         private readonly IUnitOfWorkAccessor _unitOfWorkAccessor;
         private readonly IDistributedEventBus _eventBus;
+        private readonly PaymentFakeEventService _paymentFakeEventService;
 
         public PaymentManager(
             IPaymentRepository paymentRepository,
             IUnitOfWorkAccessor unitOfWorkAccessor,
-            IDistributedEventBus eventBus)
+            IDistributedEventBus eventBus,
+            PaymentFakeEventService  paymentFakeEventService)
         {
             _paymentRepository = paymentRepository;
             _unitOfWorkAccessor = unitOfWorkAccessor;
             _eventBus = eventBus;
+            _paymentFakeEventService = paymentFakeEventService;
         }
 
         public virtual async Task<Payment> CreateAsync(Guid orderId)
@@ -33,34 +36,9 @@ namespace Shopularity.Payment.Payments
             payment = await _paymentRepository.InsertAsync(payment);
             await _unitOfWorkAccessor.UnitOfWork!.SaveChangesAsync();
             
-            _ = Task.Run(async () =>
-            {
-                await FakePaymentCompleteAsync(payment.Id);
-            });
-
+            await _paymentFakeEventService.CompletePaymentAsync(payment.Id);
+            
             return payment;
-        }
-
-        private async Task FakePaymentCompleteAsync(Guid paymentId)
-        {
-            await Task.Delay(1000 * 60);
-            
-            var payment = await _paymentRepository.GetAsync(paymentId);
-
-            if (payment.State != PaymentState.Waiting)
-            {
-                return;
-            }
-            
-            payment.State = PaymentState.Completed;
-            
-            await _paymentRepository.UpdateAsync(payment);
-
-            await _eventBus.PublishAsync(new PaymentCompletedEto
-            {
-                PaymentId = paymentId,
-                OrderId = payment.OrderId
-            });
         }
     }
 }
